55cdee3cbd05c39d805705ae1561dadf760d7247
Glysonn <kauaglyson@gmail.com>
2024-01-31 20:51:34 -0300

    Implement of Add method based on the test coverage

--- Services/CountriesService.cs
+++ Services/CountriesService.cs
@@ -1,13 +1,29 @@
-﻿using ServiceContracts;
+﻿using Entities;
+using ServiceContracts;
 using ServiceContracts.DTO;
 
 namespace Services
 {
     public class CountriesService : ICountryService
     {
+        private List<Country> _countries;
+        public CountriesService()
+        {
+            _countries = [];
+        }
         public CountryReponse AddCountry(CountryAddRequest? countryAddRequest)
         {
-            throw new NotImplementedException();
+            ArgumentNullException.ThrowIfNull(countryAddRequest, nameof(countryAddRequest));
+            ArgumentNullException.ThrowIfNull(countryAddRequest.CountryName, nameof(countryAddRequest.CountryName));
+
+            if (_countries.Exists(x => x.CountryName == countryAddRequest.CountryName))
+                throw new ArgumentException(nameof(countryAddRequest.CountryName));
+
+            var country = countryAddRequest.ToCountry();
+            country.CountryId = Guid.NewGuid();
+
+            _countries.Add(country);
+            return country.ToCountryResponse();
         }
     }
 }
