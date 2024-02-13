8e07e29dfc16fd4af9922118d8b88935b1592011
Glysonn <kauaglyson@gmail.com>
2024-02-13 17:11:28 -0300

    Implement of AddPerson method based on the test coverage

--- Services/PersonsService.cs
+++ Services/PersonsService.cs
@@ -6,14 +6,37 @@ namespace Services
 {
     public class PersonsService : IPersonService
     {
+        private readonly List<Person> _personList;
+        private readonly ICountryService _countriesService;
+        public PersonsService()
+        {
+            _personList = [];
+            _countriesService = new CountriesService();
+        }
+
         public PersonResponse AddPerson(PersonAddRequest? personToAdd)
         {
-            throw new NotImplementedException();
+            ArgumentNullException.ThrowIfNull(personToAdd, nameof(personToAdd));
+            ArgumentException.ThrowIfNullOrEmpty(personToAdd.Name, nameof(personToAdd.Name));
+
+            var person = personToAdd.ToPerson();
+            person.PersonId = Guid.NewGuid();
+
+            _personList.Add(person);
+            return ConvertPersonToPersonResponse(person);
         }
 
         public List<PersonResponse> GetAllPersons()
         {
             throw new NotImplementedException();
+        }
+
+        private PersonResponse ConvertPersonToPersonResponse(Person person)
+        {
+            var personResponse = person.ToPersonResponse();
+            personResponse.CountryName = _countriesService.GetCountryById(person.CountryId)?.CountryName;
+
+            return personResponse;
         }
     }
 }
